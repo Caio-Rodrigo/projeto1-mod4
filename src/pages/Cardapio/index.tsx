9dcd229a3d47047dc5e7fa36bf0fb60b76094c6c
Caio Rodrigo Silva <78828999+Caio-Rodrigo@users.noreply.github.com>
2022-10-02 22:16:17 -0300

    feat: introdução no cardapio filtro, buscador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -3,9 +3,13 @@ import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/My-Drink1.svg";
 import Buscador from "components/Buscador";
 import { useState } from "react";
+import Filtro from "../../components/Filtros";
+import Ordenador from "components/Ordenador";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
 
   return (
     <main>
@@ -15,11 +19,15 @@ export default function Cardapio() {
       </nav>
       <header className={styles.header}>
         <div className={styles.header__text}>
-         <span> Casa das mais divesrças bebidas</span>
+          <p> Casa das mais divesrças bebidas</p>
         </div>
         <section className={styles.cardapio}>
           <h3 className={styles.cardapio__titulo}>Cardápio</h3>
           <Buscador busca={busca} setBusca={setBusca} />
+          <div className={styles.cardapio__filtros}>
+            <Filtro filtro={filtro} setFiltro={setFiltro} />
+            <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+          </div>
         </section>
       </header>
     </main>
